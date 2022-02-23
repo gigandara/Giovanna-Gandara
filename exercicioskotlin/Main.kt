49eff0dcf8d040271474a0081539077e8be981dd
gigandara <98784790+gigandara@users.noreply.github.com>
2022-02-23 16:22:52 -0300

    Add files via upload

---
+++ exercicioskotlin/Main.kt
@@ -0,0 +1,37 @@
+fun main () {
+
+//exercicio numero 3
+
+    print("Digite sua idade.")
+    val num = readLine()!!.toInt()
+
+    when(num) {
+
+        10 ->println("Sou infantil")
+        11 ->println("Sou infantil")
+        12 ->println("Sou infantil")
+        13 ->println("Sou infantil")
+        14 ->println("Sou infantil")
+        15 ->println("Sou juvenil")
+        16 ->println("Sou juvenil")
+        17 ->println("Sou juvenil")
+        18 ->println("Sou adulto")
+        19 ->println("Sou adulto")
+        20 ->println("Sou adulto")
+        21 ->println("Sou adulto")
+        22 ->println("Sou adulto")
+        23 ->println("Sou adulto")
+        24 ->println("Sou adulto")
+        25 ->println("Sou adulto")
+
+        }
+
+
+
+
+
+
+
+
+
+    }
